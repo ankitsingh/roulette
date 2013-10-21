a837d52a887da628f3529bb109a43ab508b03be3
ankit singh <ankit.11235@gmail.com>
2013-10-21 15:43:42 +0530

    added info to setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'My Project',
+    'description': 'Simulation of roulette',
     'author': 'Ankit Singh',
-    'download_url': 'my github url',
+    'download_url': 'https://github.com/ankitsingh/roulette',
     'author_email': 'ankit.11235@gmail.com',
     'version': '0.1',
     'install_requires': ['nose'],
-    'packages': ['NAME'],
+    'packages': ['roulette'],
     'scripts': [],
-    'name': 'projectname'
+    'name': 'roulette'
 }
 
 setup(**config)
